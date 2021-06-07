9c2c87ee054935f3f31e6d32d009a9096c8bda3f
chwzr <felix.koppe93@gmail.com>
2021-06-07 13:39:50 +0200

    1 file modified, 2 added

---
+++ next.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Important: return the modified config
+    config.node = {
+      child_process: 'empty',
+      fs: 'empty',
+      crypto: 'empty',
+      net: 'empty',
+      tls: 'empty',
+      dns: 'empty'
+    };
+    return config;
+  }
+};
