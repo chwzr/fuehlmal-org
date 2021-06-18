e5d6d5acb055b6289d676242191fa4f0932552b4
chwzr <felix.koppe@cavorit.de>
2021-06-18 12:35:15 +0200

    fix login

--- pages/api/login.js
+++ pages/api/login.js
@@ -11,13 +11,13 @@ export default async (req, res) => {
     let tuser = req.body.tuser
     tuser.addedOn = Date.now();
 
-    let send = await redis.hget('logins', req.body.tuser.id);
-    if(send){
-      bot.sendMessage(req.body.tuser.id, 
-      "Hey! Danke für deinen support :] Hier wirst du alle Informationen zum fühl mal bekommen!")
-    }
+    // let send = await redis.hget('logins', req.body.tuser.id);
+    // if(send){
+      // }
+      
+      db = await redis.hset('logins', req.body.tuser.id, JSON.stringify(tuser));
 
-    db = await redis.hset('logins', req.body.tuser.id, JSON.stringify(tuser));
+      await bot.sendMessage(req.body.tuser.id, "Hey! Danke für deine Anmeldung :] Hier wirst du bald alle Informationen zum nächsten fühl mal! bekommen!")
     
 
   res.json(db);
