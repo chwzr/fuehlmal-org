46a43707737c14885cb447f977b705318eae3f52
chwzr <felix.koppe@cavorit.de>
2021-07-26 22:37:11 +0200

    fix sendmessage

--- pages/api/sendmessage.js
+++ pages/api/sendmessage.js
@@ -1,14 +1,21 @@
-import bot from '@/lib/telegram';
-
+const Slimbot = require('slimbot');
+const slimbot = new Slimbot(process.env.TELEGRAM_BOT.replace('bot',''));
+// import bot from '@/lib/telegram';
 
 export default async (req, res) => {
   // let r = JSON.parse(req.body)
 
   let resx = []
 
+  let optionalParams = {
+    parse_mode: 'Markdown'
+    };
+
   for (const id of req.body.ids) {
-    let x = await bot.sendMessage(id, req.body.message)
-    resx.push(x.data)
+    // let x = await bot.sendMessage(id, req.body.message)
+    let r = await slimbot.sendMessage(id, req.body.message, optionalParams);
+    resx.push(r)
+
   }
 
   res.json(resx);
